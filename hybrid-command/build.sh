76ff14dc26f65e110099d1c5deed2e65044d0ecf
Steve Hu <stevehu@gmail.com>
2017-07-30 10:39:16 -0400

    fixes #22 move docker-compose files to light-docker

---
+++ hybrid-command/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+IMAGE_NAME="networknt/eventuate-hybridcommand"
+TAG="${1}"
+
+if [docker images | grep -c ${IMAGE_NAME} -ge 0]; then
+	docker rmi -f ${IMAGE_NAME}
+fi
+
+docker build -t ${IMAGE_NAME} .
+docker tag ${IMAGE_NAME} ${IMAGE_NAME}:latest
+docker tag ${IMAGE_NAME} ${IMAGE_NAME}:${TAG}
+docker push ${IMAGE_NAME}
