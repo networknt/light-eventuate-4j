15f44ef5b8d7a495a38195b521567dfea77618d5
Steve Hu <stevehu@gmail.com>
2017-12-04 10:02:45 -0500

    disable integration tests in polling
    Former-commit-id: 709528b07e41bd0904fadefe7470f308d241fbed

--- eventuate-cdc-connector-polling/src/integration/java/com/networknt/eventuate/cdc/polling/AbstractPollingCdcProcessorT.java
+++ eventuate-cdc-connector-polling/src/integration/java/com/networknt/eventuate/cdc/polling/AbstractPollingCdcProcessorT.java
@@ -5,7 +5,7 @@ import com.networknt.eventuate.server.common.PublishedEvent;
 import com.networknt.eventuate.server.test.util.CdcProcessorTest;
 import com.networknt.service.SingletonServiceFactory;
 
-public abstract class AbstractPollingCdcProcessorIT extends CdcProcessorTest {
+public abstract class AbstractPollingCdcProcessorT extends CdcProcessorTest {
 
   private PollingDao<PublishedEventBean, PublishedEvent, String> pollingDao = SingletonServiceFactory.getBean(PollingDao.class);
 
