ac8a3fe1476248436dc9fc048cf8f8a86bc61ebd
Steve Hu <stevehu@gmail.com>
2017-12-04 14:46:32 -0500

    update startup hook for cdc server
    Former-commit-id: 45220dc9bd845d61fcfae0f40d375f074622ff98

--- eventuate-cdc-server/src/main/java/com/networknt/eventuate/cdc/server/CdcServerStartupHookProvider.java
+++ eventuate-cdc-server/src/main/java/com/networknt/eventuate/cdc/server/CdcServerStartupHookProvider.java
@@ -1,87 +1,21 @@
 package com.networknt.eventuate.cdc.server;
 
-import com.networknt.config.Config;
-import com.networknt.eventuate.cdc.mysql.binlog.*;
-import com.networknt.eventuate.jdbc.EventuateSchema;
-import com.networknt.eventuate.server.common.CdcConfig;
 import com.networknt.eventuate.server.common.EventTableChangesToAggregateTopicTranslator;
 import com.networknt.eventuate.server.common.PublishedEvent;
-import com.networknt.eventuate.kafka.KafkaConfig;
-import com.networknt.eventuate.kafka.producer.EventuateKafkaProducer;
-import com.networknt.eventuate.server.common.PublishedEventPublishingStrategy;
 import com.networknt.server.StartupHookProvider;
-import com.zaxxer.hikari.HikariDataSource;
-import org.apache.curator.RetryPolicy;
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.ExponentialBackoffRetry;
+import com.networknt.service.SingletonServiceFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * CdcServer StartupHookProvider. start cdc service
  */
 public class CdcServerStartupHookProvider implements StartupHookProvider {
-    static String CDC_CONFIG_NAME = "cdc";
-    static CdcConfig cdcConfig = (CdcConfig) Config.getInstance().getJsonObjectConfig(CDC_CONFIG_NAME, CdcConfig.class);
-    static String KAFKA_CONFIG_NAME = "kafka";
-    static KafkaConfig kafkaConfig = (KafkaConfig) Config.getInstance().getJsonObjectConfig(KAFKA_CONFIG_NAME, KafkaConfig.class);
-
-    static HikariDataSource dataSource;
-
-    static {
-        dataSource = new HikariDataSource();
-        dataSource.setJdbcUrl(cdcConfig.getJdbcUrl());
-        dataSource.setUsername(cdcConfig.getDbUser());
-        dataSource.setPassword(cdcConfig.getDbPass());
-        dataSource.setMaximumPoolSize(cdcConfig.getMaximumPoolSize());
-    }
-
-    public static CuratorFramework curatorFramework;
-    public static EventTableChangesToAggregateTopicTranslator<PublishedEvent> translator;
+    private static final Logger logger = LoggerFactory.getLogger(CdcServerStartupHookProvider.class);
 
     public void onStartup() {
-
-        curatorFramework = makeStartedCuratorClient(cdcConfig.getZookeeper());
-
-        SourceTableNameSupplier supplier = new SourceTableNameSupplier(cdcConfig.getSourceTableName(), "EVENTS");
-        IWriteRowsEventDataParser eventDataParser = new WriteRowsEventDataParser(dataSource, supplier.getSourceTableName(), new EventuateSchema());
-        MySqlBinaryLogClient<PublishedEvent> mySqlBinaryLogClient = new MySqlBinaryLogClient<>(
-                eventDataParser,
-                cdcConfig.getDbUser(),
-                cdcConfig.getDbPass(),
-                cdcConfig.getDbHost(),
-                cdcConfig.getDbPort(),
-                cdcConfig.getBinlogClientId(),
-                supplier.getSourceTableName(),
-                cdcConfig.getMySqlBinLogClientName()
-                );
-
-
-        EventuateKafkaProducer eventuateKafkaProducer = new EventuateKafkaProducer(kafkaConfig.getBootstrapServers());
-
-        DatabaseBinlogOffsetKafkaStore binlogOffsetKafkaStore = new DatabaseBinlogOffsetKafkaStore(
-                cdcConfig.getDbHistoryTopicName(), mySqlBinaryLogClient.getName(), eventuateKafkaProducer);
-
-        DebeziumBinlogOffsetKafkaStore debeziumBinlogOffsetKafkaStore = new DebeziumBinlogOffsetKafkaStore(
-                cdcConfig.getDbHistoryTopicName());
-
-        MySQLCdcProcessor<PublishedEvent> mySQLCdcProcessor = new MySQLCdcProcessor<>(mySqlBinaryLogClient, binlogOffsetKafkaStore, debeziumBinlogOffsetKafkaStore);
-
-        MySQLCdcKafkaPublisher<PublishedEvent> mySQLCdcKafkaPublisher = new MySQLCdcKafkaPublisher<>(binlogOffsetKafkaStore, kafkaConfig.getBootstrapServers(), new PublishedEventPublishingStrategy());
-        translator = new EventTableChangesToAggregateTopicTranslator<>(mySQLCdcKafkaPublisher, mySQLCdcProcessor, curatorFramework, cdcConfig );
+        EventTableChangesToAggregateTopicTranslator<PublishedEvent> translator = SingletonServiceFactory.getBean(EventTableChangesToAggregateTopicTranslator.class);
         translator.start();
-
-        System.out.println("CdcServerStartupHookProvider is called");
+        logger.info("CdcServerStartupHookProvider is called");
     }
-
-    CuratorFramework makeStartedCuratorClient(String connectionString) {
-        RetryPolicy retryPolicy = new ExponentialBackoffRetry(2000, 6, 2000);
-        CuratorFramework client = CuratorFrameworkFactory.
-                builder().connectString(connectionString)
-                .retryPolicy(retryPolicy)
-                .build();
-        client.start();
-        return client;
-    }
-
-
 }
