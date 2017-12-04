45220dc9bd845d61fcfae0f40d375f074622ff98
Steve Hu <stevehu@gmail.com>
2017-12-04 14:46:32 -0500

    update startup hook for cdc server

--- eventuate-cdc-connector-mysql-binlog/src/main/java/com/networknt/eventuate/cdc/mysql/binlog/MySqlCdcServiceInitializer.java
+++ eventuate-cdc-connector-mysql-binlog/src/main/java/com/networknt/eventuate/cdc/mysql/binlog/MySqlCdcServiceInitializer.java
@@ -2,9 +2,14 @@ package com.networknt.eventuate.cdc.mysql.binlog;
 
 import com.networknt.config.Config;
 import com.networknt.eventuate.jdbc.EventuateSchema;
+import com.networknt.eventuate.kafka.KafkaConfig;
 import com.networknt.eventuate.kafka.producer.EventuateKafkaProducer;
 import com.networknt.eventuate.server.common.*;
 import com.networknt.service.SingletonServiceFactory;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 
 import javax.sql.DataSource;
 import java.io.IOException;
@@ -12,15 +17,17 @@ import java.util.concurrent.TimeoutException;
 
 public class MySqlCdcServiceInitializer {
 
-    private static final String CONFIG_NAME = "cdc";
-    private static final CdcConfig config = (CdcConfig) Config.getInstance().getJsonObjectConfig(CONFIG_NAME, CdcConfig.class);
+    public static String CDC_CONFIG_NAME = "cdc";
+    public static CdcConfig cdcConfig = (CdcConfig) Config.getInstance().getJsonObjectConfig(CDC_CONFIG_NAME, CdcConfig.class);
+    public static String KAFKA_CONFIG_NAME = "kafka";
+    public static KafkaConfig kafkaConfig = (KafkaConfig) Config.getInstance().getJsonObjectConfig(KAFKA_CONFIG_NAME, KafkaConfig.class);
 
     public EventuateSchema eventuateSchema() {
         return new EventuateSchema();
     }
 
     public SourceTableNameSupplier sourceTableNameSupplier() {
-        return new SourceTableNameSupplier(config.getSourceTableName(), "EVENTS");
+        return new SourceTableNameSupplier(cdcConfig.getSourceTableName(), "EVENTS");
     }
 
     public IWriteRowsEventDataParser<PublishedEvent> eventDataParser() {
@@ -35,13 +42,13 @@ public class MySqlCdcServiceInitializer {
         IWriteRowsEventDataParser<PublishedEvent> eventDataParser = SingletonServiceFactory.getBean(IWriteRowsEventDataParser.class);
         SourceTableNameSupplier sourceTableNameSupplier = SingletonServiceFactory.getBean(SourceTableNameSupplier.class);
         return new MySqlBinaryLogClient<>(eventDataParser,
-                config.getDbUser(),
-                config.getDbPass(),
-                config.getDbHost(),
-                config.getDbPort(),
-                config.getBinlogClientId(),
+                cdcConfig.getDbUser(),
+                cdcConfig.getDbPass(),
+                cdcConfig.getDbHost(),
+                cdcConfig.getDbPort(),
+                cdcConfig.getBinlogClientId(),
                 sourceTableNameSupplier.getSourceTableName(),
-                config.getMySqlBinLogClientName());
+                cdcConfig.getMySqlBinLogClientName());
     }
 
     public EventuateKafkaProducer eventuateKafkaProducer() {
@@ -52,13 +59,13 @@ public class MySqlCdcServiceInitializer {
         MySqlBinaryLogClient<PublishedEvent> mySqlBinaryLogClient = SingletonServiceFactory.getBean(MySqlBinaryLogClient.class);
         EventuateKafkaProducer eventuateKafkaProducer = SingletonServiceFactory.getBean(EventuateKafkaProducer.class);
         return new DatabaseBinlogOffsetKafkaStore(
-                config.getDbHistoryTopicName(),
+                cdcConfig.getDbHistoryTopicName(),
                 mySqlBinaryLogClient.getName(),
                 eventuateKafkaProducer);
     }
 
     public DebeziumBinlogOffsetKafkaStore debeziumBinlogOffsetKafkaStore() {
-        return new DebeziumBinlogOffsetKafkaStore(config.getOldDbHistoryTopicName());
+        return new DebeziumBinlogOffsetKafkaStore(cdcConfig.getOldDbHistoryTopicName());
     }
 
 
@@ -67,5 +74,37 @@ public class MySqlCdcServiceInitializer {
         DatabaseBinlogOffsetKafkaStore binlogOffsetKafkaStore = SingletonServiceFactory.getBean(DatabaseBinlogOffsetKafkaStore.class);
         DebeziumBinlogOffsetKafkaStore debeziumBinlogOffsetKafkaStore = SingletonServiceFactory.getBean(DebeziumBinlogOffsetKafkaStore.class);
         return new MySQLCdcProcessor<>(mySqlBinaryLogClient, binlogOffsetKafkaStore, debeziumBinlogOffsetKafkaStore);
+    }
+
+    public CdcKafkaPublisher<PublishedEvent> mySQLCdcKafkaPublisher() {
+        DatabaseBinlogOffsetKafkaStore binlogOffsetKafkaStore = SingletonServiceFactory.getBean(DatabaseBinlogOffsetKafkaStore.class);
+        PublishingStrategy<PublishedEvent> publishingStrategy = SingletonServiceFactory.getBean(PublishingStrategy.class);
+        return new MySQLCdcKafkaPublisher<>(binlogOffsetKafkaStore, kafkaConfig.getBootstrapServers(), publishingStrategy);
+    }
+
+    public CuratorFramework curatorFramework() {
+        String connectionString = cdcConfig.getZookeeper();
+        return makeStartedCuratorClient(connectionString);
+    }
+
+    public EventTableChangesToAggregateTopicTranslator<PublishedEvent> mySqlEventTableChangesToAggregateTopicTranslator() {
+        CdcKafkaPublisher<PublishedEvent> mySQLCdcKafkaPublisher = SingletonServiceFactory.getBean(CdcKafkaPublisher.class);
+        CdcProcessor<PublishedEvent> mySQLCdcProcessor = SingletonServiceFactory.getBean(CdcProcessor.class);
+        CuratorFramework curatorFramework = SingletonServiceFactory.getBean(CuratorFramework.class);
+
+        return new EventTableChangesToAggregateTopicTranslator<>(mySQLCdcKafkaPublisher,
+                mySQLCdcProcessor,
+                curatorFramework,
+                cdcConfig);
+    }
+
+    static CuratorFramework makeStartedCuratorClient(String connectionString) {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.
+                builder().retryPolicy(retryPolicy)
+                .connectString(connectionString)
+                .build();
+        client.start();
+        return client;
     }
 }
