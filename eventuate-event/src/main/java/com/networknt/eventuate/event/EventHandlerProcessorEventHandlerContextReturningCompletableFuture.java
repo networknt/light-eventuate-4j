6f0a204f377132449f48e2fdc2f22ab5c779b8f8
Steve Hu <stevehu@gmail.com>
2017-12-06 22:13:49 -0500

    switch client side KafkaAggregateSubscriptions to jdbckafkastore in eventuate-server-jdbc

--- eventuate-event/src/main/java/com/networknt/eventuate/event/EventHandlerProcessorEventHandlerContextReturningCompletableFuture.java
+++ eventuate-event/src/main/java/com/networknt/eventuate/event/EventHandlerProcessorEventHandlerContextReturningCompletableFuture.java
@@ -3,7 +3,6 @@ package com.networknt.eventuate.event;
 import com.networknt.eventuate.common.EventHandlerContext;
 import com.networknt.eventuate.common.EventuateAggregateStore;
 
-import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Method;
 import java.util.concurrent.CompletableFuture;
 
